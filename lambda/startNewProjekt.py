6518cd00cc1ff16fa1fe0ef94e122f4031d2163e
cxnajder <98214055+cxnajder@users.noreply.github.com>
2023-04-03 21:34:26 +0200

    Update startNewProjekt.py
    To implement this code on your AWS you need to enter ARNs for separate functions

--- lambda/startNewProjekt.py
+++ lambda/startNewProjekt.py
@@ -1,23 +1,31 @@
 import boto3
 import json
 
-# key 1 = name of Cognito Pool
-# key 2 = admin adres for new Pools 
+# key 1 = name of Cognito UserPool
+# key 2 = admin's email adres for new UserPool
 
 def lambda_handler(event, context):
     lambda_client = boto3.client('lambda')
-    poolID = runLambda('arn:aws:lambda:eu-central-1:762785132751:function:AddCognitoUserPoolGit',"RequestResponse", event['key1'])
-    # runLambda(AddUserToUserPool,"RequestResponse", user_pool_ID, event['kay2']) #key2 is admni mail
-    return poolID
+    #ENTER YOUR LAMBDA ARM FOR 'AddCognitoUserPool'
+    user_pool_ID = runLambda('arn:aws:lambda:eu-central-1:762785132751:function:AddCognitoUserPoolGit',"RequestResponse", event['key1'])
+    #ENTER YOUR ARN FOR 'AddUserToUserPool'
+    runLambda('ARN',"RequestResponse", user_pool_ID, event['kay2']) #key2 is admni mail
+    #ENTER YOUR ARN FOR 'CreateGroupInPool'
+    runLambda('ARN', "RequestResponse", user_pool_ID, 'TechUser', 'technical user with higher permitions', 1) #key1: groupName; key2: description; key3: precedence;
+    #ENTER YOUR ARN FOR 'AddUserToGroup'
+    runLambda('ARN', "RequestResponse", user_pool_ID, event['key2'], 'TechUser')
+    
+    #return user_pool_ID #its for testing if function works properly
 
 
-def runLambda(lambda_name, invo_type, key1=" ", key2=" ", key3=" "):
+def runLambda(lambda_name, invo_type, key1="", key2="", key3="", key4=""):
     client = boto3.client('lambda')
 
     payload = {
         "key1": key1,
         "key2": key2,
-        "key3": key3
+        "key3": key3,
+        "key4": key4
     }
     
     response = client.invoke(
