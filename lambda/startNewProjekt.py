f4c3e5d2b9ce617c6c8ad2285af57f849a9cd479
cxnajder <98214055+cxnajder@users.noreply.github.com>
2023-04-03 20:40:31 +0200

    Update startNewProjekt.py

--- lambda/startNewProjekt.py
+++ lambda/startNewProjekt.py
@@ -2,29 +2,29 @@ import boto3
 import json
 
 def lambda_handler(event, context):
-    
-    idPool = AddCognitoUserPoolGit()
-    
-    return response
-    
 
-def AddCognitoUserPoolGit(event, context):
-    # Utwórz klienta dla pracy z AWS Lambda
+    idPool = AddCognitoUserPoolGit()
+    add(idPool)
+
+    return response
+
+user_pool_ID = runLambda(addUserPool,"", event['key1']) #key1 is userpoll name
+runLambda(adUserToPool,"", user_pool_ID, event['kay2']) #key2 is admni mail
+
+def RunLambda(lambda_name, invo_type, key1="", key2="", key3=""):
     client = boto3.client('lambda')
-    
-    # Ustawienia wywołania funkcji Lambda
-    # FunctionName - nazwa funkcji, którą chcesz wywołać
-    # InvocationType - typ wywołania. Wartość "RequestResponse" oznacza wywołanie synchroniczne
-    # Payload - dane, które chcesz przesłać do innej funkcji
+
     payload = {
-        "key": "value"
+        "key1": key1
+        "key2": key2
+        "key3": key3
     }
     response = client.invoke(
-        FunctionName="nazwa_innej_funkcji_lambda",
-        InvocationType='RequestResponse',
+        FunctionName=lambda_name,
+        InvocationType=invo_type,
         Payload=json.dumps(payload)
     )
-    
+
     # Obsługa odpowiedzi z innej funkcji Lambda
     response_payload = json.loads(response['Payload'].read().decode())
     return response_payload
