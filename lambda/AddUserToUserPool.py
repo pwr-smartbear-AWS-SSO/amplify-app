a589b97fe3df5b140192de99f57d3ce4cf83bf13
cxnajder <98214055+cxnajder@users.noreply.github.com>
2023-04-03 18:31:42 +0200

    just comments

--- lambda/AddUserToUserPool.py
+++ lambda/AddUserToUserPool.py
@@ -4,7 +4,7 @@ import json
 def lambda_handler(event, context):
     # Set the Cognito User Pool settings
     user_pool_ID = event['key1'] #getting user pool id
-    user_name = event['key2']
+    user_name = event['key2'] #the user name is the email
 
     cognito = boto3.client("cognito-idp")
     response = cognito.admin_create_user(
@@ -13,11 +13,11 @@ def lambda_handler(event, context):
         UserAttributes=[
             {
                 "Name": "email",
-                "Value": user_name
+                "Value": user_name #the user name is the email
             },
             {
                 "Name": "phone_number_verified",
-                "Value": "false"
+                "Value": "false" #no phone number verification
             }
         ],
         DesiredDeliveryMediums=[
