b4b50215f667e53b686d721b7793bd88f9dedb6a
cxnajder <98214055+cxnajder@users.noreply.github.com>
2023-04-03 21:02:34 +0200

    Update CreateGroupInPool.py

--- lambda/CreateGroupInPool.py
+++ lambda/CreateGroupInPool.py
@@ -1,14 +1,18 @@
 import boto3
 
-client = boto3.client('cognito-idp')
+cognito = boto3.client('cognito-idp')
 
 def lambda_handler(event, context):
-    user_pool_id = event['user_pool_id']
-    response = client.create_group(
-        GroupName='TechUser',
+    user_pool_id = event['key1'] #cognito pool ID
+    group_name = event['key2'] #f.e. 'TechUser'
+    description = event['key3'] #f.e. 'technical users'
+    precedence = event['key4'] #f.e. 1
+    
+    response = cognito.create_group(
+        GroupName=group_name,
         UserPoolId=user_pool_id,
-        Description='TechUser',
-        RoleArn='arn:aws:iam::762785132751:policy/testTech',
-        Precedence=1
+        Description=description,
+        #RoleArn='arn:aws:iam::762785132751:policy/testTech', #might be implemented by the enet key in the future
+        Precedence=precedence
     )
-    return response
+    return group_name
