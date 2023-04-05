a4d610de1180b16651a84957eacf1d05d5e08faa
cxnajder <98214055+cxnajder@users.noreply.github.com>
2023-04-05 09:42:06 +0200

    Update startNewProject_2_0.py

--- lambda/startNewProject_2_0.py
+++ lambda/startNewProject_2_0.py
@@ -22,7 +22,73 @@
 
 
 
-# run lambda CreateUserPool(user_pool_name) that returns {user_pool_id: xyz, client1_id: xyz, client2_id: xyz}
-# lambda CreateRole(user_pool_id) that returns iam_role_id/arn
-# lamda CreteUser(tech_user_mail, iam_role_arn) that returns tech_user_id
+# run lambda/function CreateUserPool(user_pool_name) that returns [user_pool_id, client1_id, client2_id]
+# lambda/function CreateRole(user_pool_id) that returns iam_role_id/arn
+# lamda/function CreteUser(tech_user_mail, iam_role_arn) that returns tech_user_id
 # [user_pool_id, cient1_id, client2_id, tech_user_id] >> DynamoDB
+
+
+import boto3
+import json
+
+
+def CreateUserPool(user_pool_name):
+    policies = {
+   "PasswordPolicy": {
+            "MinimumLength": 6,
+            "RequireLowercase": False,
+            "RequireNumbers": False,
+            "RequireSymbols": False,
+            "RequireUppercase": False
+        }
+    }
+    username_attributes = ["email"]
+    
+    # Create a Cognito User Pool
+    cognito_idp = boto3.client("cognito-idp")
+    response = cognito_idp.create_user_pool(
+        PoolName=user_pool_name,
+        Policies=policies,
+        UsernameAttributes=username_attributes
+    )
+
+   # Convert datetime objects to strings
+    response = json.loads(json.dumps(response, default=str))
+    user_pool_id = response["UserPool"]["Id"]
+    
+    client1_id = CreateClient(user_pool_id, 'Client1')
+    client2_id = CreateClient(user_pool_id, 'Client2')
+
+    return {
+        "user pool id": user_pool_id, 
+        "client1 id": client1_id, 
+        "client2 id": client2_id
+        }
+  
+def CreateClient(user_pool_id, client_name):
+    pass
+
+def CreateRole(user_pool_id):
+    pass
+
+def CreateUser(amplify_pool_id, user_mail, iam_role_arn):
+    pass
+
+def lambda_handler(event, context):
+    #event['key1'] is new UserPool name
+    new_user_pool_name = event['key1']
+    #event['key2'] is TechUser email
+    tech_user_mail = event['key2']
+
+    amplify_pool_id = 'XYZ' #copy-paste your amplify userpool id from AWS
+    new_user_pool_return = CreateUserPool(new_user_pool_name)
+    new_user_pool_id = new_user_pool_return["user pool id"]
+    iam_role_arn = CreateRole(new_user_pool_id)
+    tech_user_id = CreateUser(amplify_pool_id, tech_user_mail, iam_role_arn)
+
+    return {
+        "user pool id": new_user_pool_id, 
+        "client1 id": new_user_pool_return["client1 id"], 
+        "client2 id": new_user_pool_return["client2 id"], 
+        "techuser id": tech_user_id
+        }
