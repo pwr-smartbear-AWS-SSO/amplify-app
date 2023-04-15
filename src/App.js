84fb02f903aa72bbf1862e6574c2b2c6e449417b
Ghast797 <Djseweryn44@gmail.com>
2023-04-15 19:41:43 +0200

    if admin show other site

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import { Amplify} from 'aws-amplify';
+import { Amplify, Auth } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import DisplayPage from './DisplayPage';
-
 import awsExports from './aws-exports';
+import { useState, useEffect } from 'react';
+
 Amplify.configure(awsconfig);
 Amplify.configure(awsExports);
 
 function App({ signOut, user }) {
+  const [groups, setGroups] = useState([]);
+
+  useEffect(() => {
+    async function fetchGroups() {
+      const userData = await Auth.currentAuthenticatedUser();
+      setGroups(userData.signInUserSession.accessToken.payload["cognito:groups"] || []);
+    }
+    fetchGroups();
+  }, []);
+
+  const isAdmin = groups.includes("Customer_Admin_Group");
+
   return (
     <>
-      <h1>Hello {user.username}</h1>
       <button onClick={signOut}>Sign out</button>
-      <DisplayPage />
+      {isAdmin ? (
+        <DisplayPage />
+      ) : (
+        <h1>Hello {user.username}</h1>
+      )}
     </>
-    
   );
 }
 
