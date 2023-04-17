489c8fa23d41ab5c4eedec237e22f9e414f0fd30
cxnajder <98214055+cxnajder@users.noreply.github.com>
2023-04-17 08:23:41 +0200

    added debugging
    4 console.log() lines

--- src/my_code/StartNewProject.js
+++ src/my_code/StartNewProject.js
@@ -12,7 +12,9 @@ function StartNewProject() {
 
       const createuserpool_path = '/createuserpool/${value1}';
       const createuserpool_response = await API.post(api_name, createuserpool_path, {});
+      console.log(createuserpool_response); //debugging
       const new_user_pool_id = createuserpool_response.data;
+      console.log(new_user_pool_id); //debugging
       console.log('Lambda function creating user pool executed successfully.');
       
       try { //trying to create user
@@ -20,7 +22,9 @@ function StartNewProject() {
         const amplify_pool_id = "eu-central-1_DZgQ3iAsA"
         const createuser_path = '/createuser/${amplify_pool_id}/${value2}';
         const createuser_response = await API.post(api_name, createuser_path, {});
+        console.log(createuser_response); //debugging
         const new_user_id = createuser_response.data;
+        console.log(new_user_id); //debugging
         console.log('Lambda function creating user executed successfully.');
 
         try { //trying to add user to tech user group
@@ -41,7 +45,7 @@ function StartNewProject() {
           }
     
         } catch (error) { //add user to group error
-        console.error('Adding user to techusers failed!');
+          console.error('Adding user to techusers failed!');
           console.error(error);
         }
 
