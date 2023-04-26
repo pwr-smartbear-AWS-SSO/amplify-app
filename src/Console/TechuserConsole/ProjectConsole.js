84beeaaff56c3786f6d9fa32537a1650aa497702
cxnajder <cxnajder@github.com>
2023-04-26 20:13:58 +0200

    css update 2

--- src/Console/TechuserConsole/ProjectConsole.js
+++ src/Console/TechuserConsole/ProjectConsole.js
@@ -14,9 +14,10 @@ function ProjectConsole({project}){
     };
 
     return(
+        <>
         <div className='TechUserProjectConsole console_element'>
             <div className='console_element_header'>
-                <h3>{project.name}</h3>
+                <h3>{project.name} logged users</h3>
                 <div className='console_element_header_buttons'>
                     <button onClick={refreshProject}>Refresh</button>
                     <button onClick={ChangeVisibility}>{isVisible ? 'Hide Users' : 'Show Users'}</button>
@@ -28,6 +29,24 @@ function ProjectConsole({project}){
                 project_refresh_count = {project_refresh_count} />
             </div>
         </div>
+        <div className='TechUserProjectConsoleSSOManager console_element'>
+            <div className='console_element_header'>
+                <h3>Manage SSO for {project.name}</h3>
+            </div>
+            <form>
+                <lable>lable1</lable>
+                <input />
+                <br />
+                <lable>lable2</lable>
+                <input />
+                <br />
+                <lable>lable3</lable>
+                <input />
+                <br />
+                <button type="submit">Submit SSO</button>
+            </form>
+        </div>
+        </>
     )
 }
 
