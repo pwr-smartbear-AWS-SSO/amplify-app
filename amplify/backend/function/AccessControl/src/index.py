9becf83424e91e3a1931e05ca21c8dca4b336034
Anton <67004422+Korvalment@users.noreply.github.com>
2023-05-28 19:18:29 +0200

    New authorization function

--- amplify/backend/function/AccessControl/src/index.py
+++ amplify/backend/function/AccessControl/src/index.py
@@ -1,21 +1,36 @@
+import boto3
+
+
 def handler(event, context):
-    if event['authorizationToken'] == '1234':
-        auth_status = 'Allow'
-    else:
+    authorization_token = event['authorizationToken']
+    cognito_client = boto3.client('cognito-idp', region_name='eu-central-1')
+
+    try:
+        response = cognito_client.get_user(
+            AccessToken=authorization_token
+        )
+        user_id = response['Username']
+
+        if user_id in ['eu-central-1_QOoTXKgsC']:
+            auth_status = 'Allow'
+        else:
+            auth_status = 'Deny'
+    except Exception as e:
+        print(e)
         auth_status = 'Deny'
 
-    authResponse = {
+    auth_response = {
+        'principalId': user_id,
         'policyDocument': {
             'Version': '2012-10-17',
             'Statement': [
                 {
                     'Action': 'execute-api:Invoke',
-                    'Resource': [
-                        event['methodArn']
-                    ],
+                    'Resource': [event['methodArn']],
                     'Effect': auth_status
                 }
             ]
         }
     }
-    return authResponse
+
+    return auth_response
