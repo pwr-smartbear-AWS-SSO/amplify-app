9b8835ff147ee0a4be7276fa99d0f6545ecc4307
Anton <67004422+Korvalment@users.noreply.github.com>
2023-05-29 15:18:46 +0200

    Update Access lambda

--- amplify/backend/function/AccessControl/src/index.py
+++ amplify/backend/function/AccessControl/src/index.py
@@ -1,65 +1,36 @@
-import jwt
-from jwt.contrib.algorithms.pycrypto import RSAAlgorithm
+import boto3
 
+def lambda_handler(event, context):
 
-def validate_cognito_token(token, user_pool_id, region):
+    token = event['authorizationToken']
 
-    keys_url = f"https://cognito-idp.{region}.amazonaws.com/{user_pool_id}/.well-known/jwks.json"
-    response = requests.get(keys_url)
-    keys = response.json()["keys"]
-    jwt.register_algorithm("RS256", RSAAlgorithm(RSAAlgorithm.SHA256))
-    
-    decoded_token = jwt.decode(token, algorithms=["RS256"], options={"verify_signature": False})
-
-    kid = decoded_token.get("kid")
-    public_key = None
-    for key in keys:
-        if key["kid"] == kid:
-            public_key = RSAAlgorithm.from_jwk(json.dumps(key))
-            break
+    client = boto3.client('cognito-idp')
 
     try:
-        jwt.decode(token, public_key, algorithms=["RS256"])
-        return True
-    except jwt.InvalidSignatureError:
-        return False
 
-def handler(event, context):
-    token = event["authorizationToken"]
-    user_pool_id = "eu-central-1_QOoTXKgsC"
-    region = "eu-central-1"
+        response = client.get_user(AccessToken=token)
+        user_pool_id = response['UserAttributes'][0]['Value']
 
-    is_valid_token = validate_cognito_token(token, user_pool_id, region)
+        if user_pool_id == 'eu-central-1_QOoTXKgsC':
+            auth_status = 'Allow'
+        else:
+            auth_status = 'Deny'
+    except:
+        auth_status = 'Deny'
 
-    if is_valid_token:
-
-        auth_response = {
-            'principalId': 'user_id',
-            'policyDocument': {
-                'Version': '2012-10-17',
-                'Statement': [
-                    {
-                        'Action': 'execute-api:Invoke',
-                        'Resource': 'arn:aws:execute-api:region:account-id:api-id/stage/HTTP_METHOD/Resource_path',
-                        'Effect': 'Allow'
-                    }
-                ]
-            }
+    authResponse = {
+        'policyDocument': {
+            'Version': '2012-10-17',
+            'Statement': [
+                {
+                    'Action': 'execute-api:Invoke',
+                    'Resource': [
+                        event['methodArn']
+                    ],
+                    'Effect': auth_status
+                }
+            ]
         }
-        return auth_response
-    else:
+    }
 
-        auth_response = {
-            'principalId': 'user_id',
-            'policyDocument': {
-                'Version': '2012-10-17',
-                'Statement': [
-                    {
-                        'Action': 'execute-api:Invoke',
-                        'Resource': 'arn:aws:execute-api:region:account-id:api-id/stage/HTTP_METHOD/Resource_path',
-                        'Effect': 'Deny'
-                    }
-                ]
-            }
-        }
-        return auth_response
+    return authResponse
